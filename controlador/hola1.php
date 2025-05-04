be8ef9e399e11a7f221dc6dc3a8641cc25f7e813
MatiLegends <vega98790@gmail.com>
2025-05-03 21:40:06 -0300

    a los archivos .php les inclui la bd

--- controlador/hola1.php
+++ controlador/hola1.php
@@ -6,6 +6,9 @@
     <title>Document</title>
 </head>
 <body>
+    <?php
+    include('bd.php')
+    ?>
     <h1>hola1</h1>
     
 </body>
--- vista/hola2.php
+++ vista/hola2.php
@@ -6,6 +6,9 @@
     <title>Document</title>
 </head>
 <body>
+    <?php
+    include('bd.php')
+    ?>
     <h1>hola2</h1>
     
 </body>
